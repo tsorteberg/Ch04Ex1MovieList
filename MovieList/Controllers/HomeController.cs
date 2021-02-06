ff9731acbb7f2983e3daa1aeccaf0eb6993d8a27
tsorteberg <tom_sorteberg@yahoo.com>
2021-02-06 00:42:16 -0600

    Exercise 4-1 #17-19

--- MovieList/Controllers/HomeController.cs
+++ MovieList/Controllers/HomeController.cs
@@ -1,19 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using MovieList.Models;
 
 namespace MovieList.Controllers
 {
     public class HomeController : Controller
     {
+        private MovieContext context { get; set; }
+
+        public HomeController(MovieContext ctx) 
+        {
+            context = ctx;
+        }
         public IActionResult Index()
         {
-            return View();
+            var movies = context.Movies.OrderBy(m => m.Name).ToList();
+            return View(movies);
         }
     }
 }
